1e3ecfc50db5a632607df77f9b6ed1fd0b454ff8
AHMEDNABIB <nabibpallab@gmail.com>
2024-01-27 16:11:27 +0600

    change banner and pay bill image

--- src/pages/Paybill/Paybill/Paybill.jsx
+++ src/pages/Paybill/Paybill/Paybill.jsx
@@ -5,9 +5,11 @@ import { BsClipboard2DataFill } from "react-icons/bs";
 import { BsClipboardCheckFill } from "react-icons/bs";
 import { TbCoinTakaFilled } from "react-icons/tb";
 import BankPay from "../BankPay/BankPay";
-import AOS from "aos";
+import AOS from "aos"; 
 import "aos/dist/aos.css";
 import Heading from "../Heading/Heading";
+
+import bkash from "../../../assets/image/bkash.jpg";
 
 const Paybill = () => {
 	useEffect(() => {
@@ -22,7 +24,7 @@ const Paybill = () => {
 
 	return (
 		<div>
-			<Heading/>
+			<Heading />
 			<div className="hero mt-8 ">
 				<div className="hero-content text-center flex-col ">
 					<div className="max-w-xl">
@@ -31,8 +33,8 @@ const Paybill = () => {
 						</h1>
 					</div>
 					<div className="">
-						<div className="container mx-auto mt-12">
-							<div className="flex flex-col items-center justify-center max-w-sm">
+						<div className="container  mt-12">
+							<div className="flex flex-col items-center justify-between max-w-sm">
 								<ul className="flex space-x-2">
 									<li>
 										<a
@@ -59,144 +61,158 @@ const Paybill = () => {
 										</a>
 									</li>
 								</ul>
-								<div className="p-3 mt-6 bg-white" >
+								<div className="p-3 mt-6 bg-white">
 									<div
 										className={
 											openTab === 1 ? "block" : "hidden"
 										}>
 										{" "}
-										<ul
-											className="timeline timeline-vertical items-start lg:ml-[-200px] ml-[-380px] w-96"
-											>
-											<li>
-												<div className="timeline-middle">
-													<FaBackward className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Login to your bKash account
-													with your account's PIN
-													number and tap on "Pay Bill"
-													option.
-												</div>
-												<hr />
-											</li>
-											<li >
-												<hr />
-												<div className="timeline-middle">
-													<FaLightbulb className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													In "Pay Bill" option you'll
-													find "Stargate Internet".
-													Simply tap on it and follow
-													the next step.
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<BsClipboard2DataFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Now Enter Customer Id and
-													Customer Contact Number.Tap
-													on it and follow the next
-													step.
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<BsClipboardCheckFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Enter Amount of money you
-													wan to pay
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<TbCoinTakaFilled className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													After that, hold the pay
-													button for a few second.
-													You're done!
-												</div>
-											</li>
-										</ul>
+										<div className="flex items-center justify-between -ml-80 ">
+											<ul className="timeline timeline-vertical ">
+												<li>
+													<div className="timeline-middle">
+														<FaBackward className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Login to your bKash
+														account with your
+														account's PIN number and
+														tap on "Pay Bill"
+														option.
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<FaLightbulb className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														In "Pay Bill" option
+														you'll find "Stargate
+														Internet". Simply tap on
+														it and follow the next
+														step.
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<BsClipboard2DataFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Now Enter Customer Id
+														and Customer Contact
+														Number.Tap on it and
+														follow the next step.
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<BsClipboardCheckFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Enter Amount of money
+														you wan to pay
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<TbCoinTakaFilled className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														After that, hold the pay
+														button for a few second.
+														You're done!
+													</div>
+												</li>
+											</ul>
+
+											<img
+												className="ml-[400px] "
+												src={bkash}
+												alt=""
+											/>
+										</div>
 									</div>
 									<div
 										className={
 											openTab === 2 ? "block" : "hidden"
-										} >
-										<ul className="timeline timeline-vertical items-start lg:ml-[-200px] ml-[-380px]">
-											<li>
-												<div className="timeline-middle">
-													<FaBackward className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Login to your Nagad account
-													with your account's PIN
-													number and tap on "Pay Bill"
-													option.
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<FaLightbulb className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													In "Pay Bill" click internet
-													option you'll find "Stargate
-													Internet". Simply tap on it
-													and follow the next step.
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<BsClipboard2DataFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Now Enter Customer Id Tap on
-													it and follow the next step.
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<BsClipboardCheckFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Enter Amount of money you
-													wan to pay and give
-													reference Name and follow
-													the next step
-												</div>
-												<hr />
-											</li>
-											<li>
-												<hr />
-												<div className="timeline-middle">
-													<TbCoinTakaFilled className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
-												</div>
-												<div className="timeline-end timeline-box w-96">
-													Enter the pin number .After
-													that , hold the pay button
-													for a few second. You're
-													done!
-												</div>
-											</li>
-										</ul>
+										}>
+										<div>
+											<ul className="timeline timeline-vertical items-start lg:ml-[-200px] ml-[-380px]">
+												<li>
+													<div className="timeline-middle">
+														<FaBackward className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Login to your Nagad
+														account with your
+														account's PIN number and
+														tap on "Pay Bill"
+														option.
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<FaLightbulb className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														In "Pay Bill" click
+														internet option you'll
+														find "Stargate
+														Internet". Simply tap on
+														it and follow the next
+														step.
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<BsClipboard2DataFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Now Enter Customer Id
+														Tap on it and follow the
+														next step.
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<BsClipboardCheckFill className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Enter Amount of money
+														you wan to pay and give
+														reference Name and
+														follow the next step
+													</div>
+													<hr />
+												</li>
+												<li>
+													<hr />
+													<div className="timeline-middle">
+														<TbCoinTakaFilled className="text-[#fff] bg-primary rounded-full   text-3xl p-2  " />
+													</div>
+													<div className="timeline-end timeline-box w-96">
+														Enter the pin number
+														.After that , hold the
+														pay button for a few
+														second. You're done!
+													</div>
+												</li>
+											</ul>
+										</div>
 									</div>
 								</div>
 							</div>
